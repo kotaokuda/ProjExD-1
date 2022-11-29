e426ea9ad91b29e517fe6b840410587b1fbb370e
c0a21081 <c0a2108119@edu.teu.ac.jp>
2022-11-29 16:42:16 +0900

    う””－ん

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,33 +1,121 @@
 import tkinter as tk
-import tkinter.messagebox as tkm
 
-def button_click(event):
+def button_click(event,str = "None"):
+    global mark
     btn = event.widget
-    num = btn["text"]
-    enrty.insert(tk.END, num)
+    if(str=="None"):
+        num = btn["text"]
+    else:
+        num = str
+    if(mark):
+        entry.insert(tk.END, num)
+    else:
+        if(num in ng):
+            mark = False
+        else:
+            mark = True
+            entry.insert(tk.END, num)
+    if(num in ng):
+        mark = False
 
 def calc_num(event):
-    calc = enrty.get()
+    calc = entry.get()
+    for i, eo in enumerate(evaloperation):
+        calc = calc.replace(operations[i],eo)
+    calc = calc.replace("^","**")
+    calc = calc.replace("√","**(1/2)")
+    calc = calc + ")"*kaca
     result = eval(calc)
-    enrty.delete(0,tk.END)
-    enrty.insert(tk.END, result)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, result)
+
+def delate_num(event):
+    den = entry.get()
+    entry.delete(len(den) - 1)
+
+def c_delate(event):
+    entry.delete(0,tk.END)
+
+def percent(event):
+    per = entry.get()
+    eper = eval(per)
+    entry.delete(0,tk.END)
+    entry.insert(tk.END, eper/100)
+
+def tx(event):
+    txen = entry.get()
+    if(len(txen)>0):
+        tlist = list(txen)
+        if(tlist[-1] in ng):
+            return
+        else:
+            tt = txen 
+            for op in operations:
+                 tt= tt.replace(op," ")
+            tt = tt.split(" ")
+            entry.delete(len(txen)-len(tt[-1]),tk.END)
+            entry.insert(tk.END,1/int(tt[-1]))
+
+def kako(event):
+    global kaca
+    entry.insert(tk.END, "(")
+    kaca += 1
+
+def kakotoji(event):
+    global kaca
+    entry.insert(tk.END, ")")
+    kaca -= 1
+
+def x2(event):
+    button_click(event, "^")
+
+def rootx(event):
+    button_click(event, "√")
 
 root = tk.Tk()
 root.geometry("300x500")
 
-enrty = tk.Entry(root, justify = "right", width = 10, font = ("", 40))
-enrty.grid(row = 0, column = 0, columnspan = 3)
+
+f = 20
+w = 4
+h = 1
+mark = False
+kaca = 0
+
+entry = tk.Entry(root, justify = "right", width = 10, font = ("", 40))
+entry.grid(row = 0, column = 0, columnspan = 4)
+
+command = ["%", "1/x","C", "[X]", "x^2", "√x", "(", ")"]
+binds = [percent, tx, c_delate, delate_num, x2, rootx, kako,kakotoji]
+for j,c in enumerate(command):
+    bc = tk.Button(root, text = c, width = w, height = h, font = ("", f))
+    bc.bind("<1>", binds[j])
+    bc.grid(row = 1+j//4, column = j%4)
 
 for i in range(9, -1, -1):
-    b = tk.Button(root, text = 9-i, width = 4, height = 2, font = ("", 30))
+    b = tk.Button(root, text = 9-i, width = w, height = h, font = ("", f))
     b.bind("<1>", button_click)
-    b.grid(row = 1+i//3, column = i%3)
+    if(i != 9):
+        b.grid(row = 3+i//3, column = 2-i%3)
+        continue
+    b.grid(row = 6, column = 0)
 
-buttonP = tk.Button(root, text = "+", width = 4, height = 2, font = ("", 30))
-buttonP.bind("<1>", button_click)
-buttonP.grid(row = 4, column = 1 )
-buttonE = tk.Button(root, text = "=", width = 4, height = 2, font = ("", 30))
+dot = tk.Button(root, text = ".", width = w, height = h, font = ("", f))
+dot.bind("<1>", button_click)
+dot.grid(row = 6, column = 1)
+
+operations = ["÷", "×", "-", "+"]
+evaloperation = ["/", "*"]
+
+for i, o in enumerate(operations):
+    bo = tk.Button(root, text = o, width = w, height = h, font = ("", f)) 
+    bo.bind("<1>", button_click)
+    bo.grid(row = 3+i, column = 3 )
+
+buttonE = tk.Button(root, text = "=", width = w, height = h, font = ("", f))
 buttonE.bind("<1>", calc_num)
-buttonE.grid(row = 4, column = 2)
+buttonE.grid(row = 6, column = 2)
+
+ng = operations+[".","^"]
 
 root.mainloop()
