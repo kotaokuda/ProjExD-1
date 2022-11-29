4c0ab2133cc8f554a2c33fddfc0f04424c45210e
c0a21081 <c0a2108119@edu.teu.ac.jp>
2022-11-29 14:19:49 +0900

    ex02-e2

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,4 +3,9 @@ import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.geometry("300x500")
+
+for i in range(9, -1, -1):
+    b = tk.Button(root, text = 9-i, width = 4, height = 2, font = ("", 30))
+    b.grid(row = i//3, column = i%3)
+
 root.mainloop()
