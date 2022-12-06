2df460860c2c16cdb92e776dd484a1d5feb1b19b
c0a21081 <c0a2108119@edu.teu.ac.jp>
2022-12-06 14:35:37 +0900

    e3-10

--- ex03/maze.py
+++ ex03/maze.py
@@ -24,16 +24,17 @@ def key_down(event):
     print(key)
 
 if __name__ == "__main__":
-    cx = 300
-    cy = 400
+    cx = 150
+    cy = 150
     key = ""
     root = tk.Tk()
     root.title("迷えるこうかとん")
     Canvas = tk.Canvas(root, width=1500, height=900, bg="black")
+    maze = mm.make_maze(15,9)
+    mm.show_maze(Canvas, maze)
     image = tk.PhotoImage(file="fig/0.png")
     Canvas.create_image(cx, cy, image=image, tag="Kouka")
     Canvas.pack()
-    maze = mm.make_maze(15,9)
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     root.mainloop()
