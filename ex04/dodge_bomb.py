dc2d799fb70e9af412c89db51d1ad22359bb8fc2
c0a21081 <c0a2108119@edu.teu.ac.jp>
2022-12-13 14:10:51 +0900

    e4-2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -8,6 +8,16 @@ def main():
     scrn_sfc = pg.display.set_mode((1600,900))
     
     back_sfc = pg.image.load("fig/pg_bg.jpg")
+    back_rct = back_sfc.get_rect()
+    back_rct.center = 800, 450
+    scrn_sfc.blit(back_sfc, back_rct)
+
+    pg.display.update()
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        pg.display.update()
+        clock.tick(1000)
 
 if __name__ == "__main__":
     pg.init()
