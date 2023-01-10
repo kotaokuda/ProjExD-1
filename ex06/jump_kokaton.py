5fb3393e32db0464fcc87c9c4c6bee73c78ec946
nisiyama <c0a21166e1@edu.teu.ac.jp>
2023-01-10 18:47:25 +0900

    c0a21166

--- ex06/jump_kokaton.py
+++ ex06/jump_kokaton.py
@@ -1,9 +1,10 @@
 import pygame as pg
 import random
 import sys
+import os
 
 
-class Screen:#背景を生成
+class Screen:#背景
     def __init__(self, title, whtpl, bgfile):
         self.title = title
         self.whtpl = whtpl
@@ -13,33 +14,42 @@ class Screen:#背景を生成
         self.bgi_sfc = pg.image.load(bgfile)
         self.bgi_rct = self.bgi_sfc.get_rect()
  
-    def blit(self):
+    def blit(self): #出力
         self.sfc.blit(self.bgi_sfc, self.bgi_rct)
 
 
-class Bird:#こうかとんを生成
-    def __init__(self, figfile, zoom, center):
+class Bird:#こうかとん
+    def __init__(self, figfile, zoom, center,sp):
         self.sfc = pg.image.load(figfile)
         self.sfc = pg.transform.rotozoom(self.sfc, 0, zoom)
         self.sfc = pg.transform.flip(self.sfc, True, False) #向きを反転
         self.rct = self.sfc.get_rect()
         self.rct.center = center
+        self.sp = sp    
+        self.kis = False
 
-    def blit(self, scr):
+    def blit(self, scr): #出力
         scr.sfc.blit(self.sfc, self.rct)
 
+    #スペースを押したときにこうかとんが跳ねる関数（c0a21166が改良）
     def update(self, scr):
         key_dct = pg.key.get_pressed()
-        self.rct.centery += 2
-        if key_dct[pg.K_SPACE]:
-            for _ in range(7):
-                self.rct.centery += -0.1
-                if self.rct.top < scr.rct.top:
-                    self.rct.centery += 0.1
-        scr.sfc.blit(self.sfc, self.rct) # 練習3
+        if key_dct[pg.K_SPACE] and self.kis == False: #加速度決め（連続押しは不可）
+            self.kis =True
+            self.sp = -4
+        else:
+            self.sp += 0.1
+            if self.sp > 5:
+                self.sp = 5
+        if not key_dct[pg.K_SPACE]: #スペースを押してないときに跳ねる機構を回復
+            self.kis = False
+        self.rct.centery += self.sp
+        if self.rct.top < scr.rct.top: #天井を叩いたとき、画面外に行かない処理
+            self.rct.centery += scr.rct.top - self.rct.top 
+        scr.sfc.blit(self.sfc, self.rct) #書き込み
 
 
-class Wall:
+class Wall:#壁
     def __init__(self):
         self.top = random.randint(0, 6)
         self.sfc1 = pg.Surface((100, self.top * 100))
@@ -53,85 +63,44 @@ class Wall:
         self.rct2 = self.sfc2.get_rect() 
         self.rct2.center = (1550, 600 + self.top * 50)
 
-
     def blit(self, scr):
         scr.sfc.blit(self.sfc1, self.rct1)
         scr.sfc.blit(self.sfc2, self.rct2)
 
-    def update(self, scr):
+    def update(self, scr): #位置の移動
         self.rct1.move_ip(-1, 0)
         self.rct2.move_ip(-1, 0)
         self.blit(scr)
 
-class Button:#ボタン用imageの生成 C0A21081作成
-    def __init__(self, figfile, center):
-        self.sfc = pg.image.load(figfile)
-        self.sfc = pg.transform.rotozoom(self.sfc, 0, 2)
-        self.sfc = pg.transform.flip(self.sfc, True, False) #向きを反転
-        self.rct = self.sfc.get_rect()
-        self.rct.center = center
 
-    def blit(self, scr):
-        scr.sfc.blit(self.sfc, self.rct)
+#スコアをテキストファイルに記入する関数（c0a21166が作成）
+def score():
+    score = str(pg.time.get_ticks() / 1000)
+    if os.path.exists("score.txt") == False:
+        with open("score.txt","w",encoding="utf_8") as f:
+            f.write(score)
+    else:
+        with open("score.txt","r",encoding="utf_8") as f:
+            line = f.read()
+            if float(line) < float(score):
+                with open("score.txt","w",encoding="utf_8") as f:
+                    f.write(score)
 
-    def update(self, scr):
-        self.blit(scr)
 
-
+#メイン
 def main():
     global game
     time = 0
-    Start = True
 
     clock =pg.time.Clock()
 
-    scr = Screen("飛べ！こうかとん", (1600, 900), "fig/pg_bg.jpg") #C0A21081作成↓
+    scr = Screen("飛べ！こうかとん", (1600, 900), "fig/pg_bg.jpg")
     scr.blit()
-    
-    kbn_start = Button("fig/3.png", (400, 450))#スタートボタンを生成
-    kbn_start.blit(scr)
-    kbn_exit = Button("fig/2.png", (1200, 450))#終了ボタンを生成
-    kbn_exit.blit(scr)
-    start = pg.font.Font(None, 100)
-    exit = pg.font.Font(None, 100)
-    txt_s = start.render("START", True, "black")
-    txt_e = exit.render("EXIT", True, "black")
-    scr.sfc.blit(txt_s, (kbn_start.rct.width, kbn_start.rct.height)) 
-    scr.sfc.blit(txt_e, (kbn_exit.rct.width, kbn_exit.rct.height)) 
 
-
-    while Start:
-        scr.blit()
-
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                game = False
-                return
-            elif event.type == pg.MOUSEBUTTONUP:
-                posx, posy = event.pos
-                if kbn_start.rct.left < posx and posx < kbn_start.rct.right:
-                    if kbn_start.rct.bottom > posy and posy > kbn_start.rct.top:#画像範囲内をクリックしたら反応
-                        Start =False#スタート画面のwhileを脱出
-                elif kbn_exit.rct.left < posx and posx < kbn_exit.rct.right:
-                    if kbn_exit.rct.bottom > posy and posy > kbn_exit.rct.top:#画像範囲内をクリックしたら反応
-                        game = False
-                        return
-
-            
-        kbn_start.update(scr)
-        kbn_exit.update(scr)
-
-        txt_s = start.render("START", True, "black")
-        txt_e = exit.render("EXIT", True, "black")
-        scr.sfc.blit(txt_s, (kbn_start.rct.centerx - 100, kbn_start.rct.centery + 50)) 
-        scr.sfc.blit(txt_e, (kbn_exit.rct.centerx - 100, kbn_exit.rct.centery + 50)) 
-
-        pg.display.update() #C0A21081作成↑
-
-    kkt = Bird("fig/3.png", 2.0, (scr.whtpl[0]/2, scr.whtpl[1]/2))
+    kkt = Bird("fig/3.png", 2.0, (scr.whtpl[0]/2, scr.whtpl[1]/2),0)
     kkt.blit(scr)
 
-    wlls = [Wall()]    
+    wlls = [Wall()]
     wlls[0].blit(scr)
 
     while True:
@@ -153,20 +122,23 @@ def main():
                 wlls.remove(wll)
 
             if kkt.rct.colliderect(wll.rct1) or kkt.rct.colliderect(wll.rct2):
+                score() 
                 return
         
         if kkt.rct.bottom > scr.rct.bottom:
+            score()
             return
     
         pg.display.update()
         time += 1
         clock.tick(1000)
+    
 
-
+#本体
 if __name__ == "__main__":
     game = True
     pg.init() # 初期化
-    while game:
-        main() # ゲームの本体
+    main() # ゲームの本体
+    #pg.time.wait(1000) #テスト時にスペース連打でコードがぐちゃぐちゃになるのをケア
     pg.quit() # 初期化の解除
     sys.exit()
